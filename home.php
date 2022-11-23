582e8839baa8c5758c0dd1adf941632e22358b2e
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-11-23 23:00:08 +0200

    prepare files structure

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+    <!-- Swiper Link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+    <!-- Font Awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <!-- Css File Link-->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <!--swiper js link-->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <!--JavaScript File Link-->
+    <script src="js/script.js"></script>
+</body>
+</html>
