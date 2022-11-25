078d571332d0ef4333bcd94f512010dbebc239eb
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-11-25 19:01:52 +0200

    ending header section

--- home.php
+++ home.php
@@ -4,20 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World!</title>
-    <!-- Swiper Link -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
-    <!-- Font Awesome cdn link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <!-- Css File Link-->
-    <link rel="stylesheet" href="css/style.css">
+    <title>home</title>
 
+    <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>" type="text/css">
 </head>
 <body>
-    <h1>Hello World!</h1>
-    <!--swiper js link-->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-    <!--JavaScript File Link-->
-    <script src="js/script.js"></script>
+    <!-- Header Section Start -->
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <!-- Header Section End -->
+    <!-- swiper js link  -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+    <!-- custom js file link  -->
+    <script type="text/javascript" src="js/script.js?v=<?php echo time();?>"></script>
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*--- Start Scripting ---*/
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+     menu.classList.toggle('fa-times');
+     navbar.classList.toggle('active');
+}
