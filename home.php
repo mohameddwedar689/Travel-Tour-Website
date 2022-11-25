ac38bf0107ea68ee5c358fc181edfe7113371dbf
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-11-25 19:57:21 +0200

    end servcies section

--- home.php
+++ home.php
@@ -30,7 +30,42 @@
         <div id="menu-btn" class="fas fa-bars"></div>
     </section>
     <!-- Header Section End -->
-    <!-- Footer Section Start -->
+    <!-- Services Section Start-->
+    <section class="services">
+        <h1 class="heading-title"> our services </h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="images/icon-1.png" alt="">
+                <h3>adventure</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-2.png" alt="">
+                <h3>tour guide</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-3.png" alt="">
+                <h3>trekking</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-4.png" alt="">
+                <h3>camp fire</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-5.png" alt="">
+                <h3>off road</h3>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-6.png" alt="">
+                <h3>camping</h3>
+            </div>
+        </div>
+    </section>
+    <!-- Services Section End -->
     <section class="footer">
         <div class="box-container">
             <div class="box">
