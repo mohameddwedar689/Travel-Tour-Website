33bc90f8ef79a85f263663d5e54dab9dd5afa556
mohameddwedar689 <mohameddwedar689@gmail.com>
2022-12-01 13:37:43 +0200

    finish booking form section

--- book.php
+++ book.php
@@ -34,6 +34,56 @@
     <div class="heading" style="background: url(images/header-bg-3.png) no-repeat">
         <h1>Book</h1>
     </div>
+
+    <section class="booking">
+        <h1 class="heading-title">book your trip!</h1>
+
+        <form action="book_form.php" method="post" class="book-form">
+            <div class="flex">
+                <div class="inputBox">
+                    <span>name :</span>
+                    <input type="text" placeholder="enter your name" name="name">
+                </div>
+
+                <div class="inputBox">
+                    <span>email :</span>
+                    <input type="email" placeholder="enter your email" name="email">
+                </div>
+
+                <div class="inputBox">
+                    <span>phone :</span>
+                    <input type="number" placeholder="enter your number" name="phone">
+                </div>
+
+                <div class="inputBox">
+                    <span>address :</span>
+                    <input type="text" placeholder="enter your address" name="address">
+                </div>
+
+                <div class="inputBox">
+                    <span>where to :</span>
+                    <input type="text" placeholder="place you want to visit" name="location">
+                </div>
+
+                <div class="inputBox">
+                    <span>how many :</span>
+                    <input type="number" placeholder="number of guests" name="guests">
+                </div>
+
+                <div class="inputBox">
+                    <span>arrivals :</span>
+                    <input type="date" name="arrivals">
+                </div>
+
+                <div class="inputBox">
+                    <span>leaving :</span>
+                    <input type="date" name="leaving">
+                </div>
+            </div>
+
+            <input type="submit" value="submit" class="btn" name="send">
+        </form>
+    </section>
     <!-- Book Section End-->
     <!-- Footer Section Start -->
     <section class="footer">
